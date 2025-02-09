b65dc9c8eeec668c207a0d40e43866cc98fc73f6
boramchoi <bchoe324@gmail.com>
2025-02-09 12:34:49 +0900

    💄 date picker css 추가

--- src/components/tickets/TicketInfoComponents.tsx
+++ src/components/tickets/TicketInfoComponents.tsx
@@ -112,6 +112,17 @@ export const Content = styled.div`
     font-size: 18px;
     font-weight: 500;
   }
+  .review {
+    flex-direction: column;
+    align-items: flex-start;
+    .text,
+    textarea {
+      width: 100%;
+      resize: none;
+    }
+  }
+
+  /* date_picker 스타일 */
   .date_picker {
     flex: 4 1 40px;
     .react-datepicker-wrapper {
@@ -124,14 +135,46 @@ export const Content = styled.div`
         padding: 0;
       }
     }
-  }
-  .review {
-    flex-direction: column;
-    align-items: flex-start;
-    .text,
-    textarea {
-      width: 100%;
-      resize: none;
+    .react-datepicker {
+      border: 1px solid #ccc;
+      border-radius: 0.8rem;
+      overflow: hidden;
+      box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    }
+    .react-datepicker__navigation-icon::before,
+    .react-datepicker__year-read-view--down-arrow,
+    .react-datepicker__month-read-view--down-arrow,
+    .react-datepicker__month-year-read-view--down-arrow {
+      border-width: 2px 2px 0 0;
+    }
+    .react-datepicker__triangle {
+      display: none;
+    }
+    .react-datepicker__header {
+      background-color: #fff;
+      border-bottom: 1px solid #ccc;
+    }
+    .react-datepicker__time-container {
+      border-left: 1px solid #ccc;
+    }
+    .react-datepicker__time-container
+      .react-datepicker__time
+      .react-datepicker__time-box
+      ul.react-datepicker__time-list
+      li.react-datepicker__time-list-item--selected,
+    .react-datepicker__day--selected,
+    .react-datepicker__day--in-selecting-range,
+    .react-datepicker__day--in-range,
+    .react-datepicker__month-text--selected,
+    .react-datepicker__month-text--in-selecting-range,
+    .react-datepicker__month-text--in-range,
+    .react-datepicker__quarter-text--selected,
+    .react-datepicker__quarter-text--in-selecting-range,
+    .react-datepicker__quarter-text--in-range,
+    .react-datepicker__year-text--selected,
+    .react-datepicker__year-text--in-selecting-range,
+    .react-datepicker__year-text--in-range {
+      background-color: #813dff;
     }
   }
 `;
