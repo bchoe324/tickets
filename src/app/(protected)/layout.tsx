f952bbc355903cef3717a4099bcdbf5b565f83f2
boramchoi <bchoe324@gmail.com>
2025-05-02 14:19:33 +0900

    add layout and page components for user profile, ticket management, and reviews; remove obsolete middleware

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,15 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+
+export default async function ProtectedLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const accessToken = (await cookies()).get("access_token");
+
+  if (!accessToken) redirect("/login");
+
+  return <>{children}</>;
+}
