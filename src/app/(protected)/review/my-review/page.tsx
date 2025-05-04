56a3eaea59ca53972e0c7aa6605d3b7375855b23
boramchoi <bchoe324@gmail.com>
2025-05-05 01:09:34 +0900

    내 리뷰에 스켈레톤 추가

--- src/app/(protected)/review/my-review/page.tsx
+++ src/app/(protected)/review/my-review/page.tsx
@@ -7,25 +7,36 @@ import { ReviewData } from "@/types";
 import Link from "next/link";
 import Image from "next/image";
 import plus from "@/assets/icons/plus.svg";
+import { Suspense } from "react";
+import ReviewItemSkeleton from "@/components/skeleton/review-item-skeleton";
+
+async function ReviewList() {
+  const reviews: ReviewData[] = await fetchReviewData("myreview");
+  return (
+    <>
+      {reviews.length > 0 ? (
+        reviews.map((review) => (
+          <div className="review-item-layout" key={review.createdAt}>
+            <ReviewItem review={review} isMenuVisible={true} />
+          </div>
+        ))
+      ) : (
+        <NoContent>
+          <p>작성한 관람 후기가 없어요.</p>
+        </NoContent>
+      )}
+    </>
+  );
+}
 
 export default async function Page() {
-  const reviews: ReviewData[] = await fetchReviewData("myreview");
-
   return (
     <>
       <DetailHeader centerChild={"내 관람 후기"} />
       <main>
-        {reviews.length > 0 ? (
-          reviews.map((review) => (
-            <div className="review-item-layout" key={review.createdAt}>
-              <ReviewItem review={review} isMenuVisible={true} />
-            </div>
-          ))
-        ) : (
-          <NoContent>
-            <p>작성한 관람 후기가 없어요.</p>
-          </NoContent>
-        )}
+        <Suspense fallback={<ReviewItemSkeleton count={5} />}>
+          <ReviewList />
+        </Suspense>
       </main>
       <aside>
         <FloatingButton>
