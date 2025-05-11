256d376905f73baea8a83418b74e55bfcd8fe356
boramchoi <bchoe324@gmail.com>
2025-05-12 00:11:01 +0900

    access token 갱신

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,6 +24,36 @@ export async function middleware(req: NextRequest) {
   }
 
   // access token이 없고 refresh token이 있음 = access token 재발급 요청
+  if (refreshToken) {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_SERVER_URL}/auth/refresh`,
+        {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            Authorization: `Bearer ${refreshToken}`,
+          },
+        }
+      );
+      if (!response.ok) throw new Error("refreshing token failed");
+      const { newAccessToken } = await response.json();
+      if (newAccessToken) {
+        const res = NextResponse.next();
+        res.cookies.set("access_token", newAccessToken, {
+          httpOnly: true,
+          path: "/",
+          maxAge: 60 * 15,
+          secure: true,
+          sameSite: "strict",
+        });
+        return res;
+      }
+    } catch (error) {
+      console.error("Error refreshing access token", error);
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  }
 
   // access token과 refresh token이 없음 = 로그인 안된 상태
   if (!isAuthPage) {
